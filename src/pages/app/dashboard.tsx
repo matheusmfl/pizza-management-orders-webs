652574b6ab4ef4edc281297cb8157523c4471742
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-03 23:33:44 -0300

    feat: add helmet extension on project

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
-  return <h1>dashboard</h1>
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1>Dashboard</h1>
+    </>
+  )
 }
