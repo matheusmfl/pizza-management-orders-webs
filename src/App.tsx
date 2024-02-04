dc97c4799a3a5a3a4580805b381c34553f1f6542
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-03 23:21:36 -0300

    feat: add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex justify-center items-center h-screen ">
-      <Button>Enviar</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,3 @@
+export function Dashboard() {
+  return <h1>dashboard</h1>
+}
