b88f68d60839b534d404b77af35890ab7747ce3f
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-03 22:50:29 -0300

    feat: config plugin for the eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-
-
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="flex justify-center items-center h-screen ">
+      <Button>Enviar</Button>
+    </div>
   )
 }
-
