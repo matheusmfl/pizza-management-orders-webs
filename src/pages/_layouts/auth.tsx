4c854e6f464d2f4a56848ffdc712d7c2706cb0bd
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-16 23:25:15 -0300

    feat: add page auth

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
+import { Pizza } from "lucide-react";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Authentication</h1>
+    <div className="min-h-screen grid grid-cols-2 ">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">
+            pizza.shop
+          </span>
+        </div>
+        <footer>
+          {" "}
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
-  )
+  );
 }
