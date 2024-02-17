e5c73e85b0ce69599a987fe44fa33cf57013114e
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-16 23:24:42 -0300

    refactor: rename imports

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/auth'
+import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
