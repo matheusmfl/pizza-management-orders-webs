befc6ad5a41bbde1bd8a0eb380f810425c822e7b
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-03-03 20:43:55 -0300

    feat: add link-component , add separator, add layout to app

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,18 +1,16 @@
-import { Outlet } from "react-router-dom";
-import { Pizza } from "lucide-react";
+import { Pizza } from 'lucide-react'
+import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="min-h-screen grid grid-cols-2 ">
+    <div className="min-h-screen grid grid-cols-2 antialiased">
       <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5" />
-          <span className="font-semibold">
-            pizza.shop
-          </span>
+          <span className="font-semibold">pizza.shop</span>
         </div>
         <footer>
-          {" "}
+          {' '}
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
@@ -21,5 +19,5 @@ export function AuthLayout() {
         <Outlet />
       </div>
     </div>
-  );
+  )
 }
